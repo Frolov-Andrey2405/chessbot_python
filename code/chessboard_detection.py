7f0bf6791773c354a182e8a38b856e7ca8d8dd8f
Andrey Frolov <senior.slender2015@gmail.com>
2024-06-18 11:43:52 +0300

    ruff

--- code/chessboard_detection.py
+++ code/chessboard_detection.py
@@ -1,70 +1,86 @@
-import numpy as np
-import cv2 #OpenCV
-import pyautogui #Used to take screenshots and move the mouse
-import chess #This is used to deal with the advancement in the game
-import chess.uci #This is used to transform uci notations: for instance the uci "e2e4" corresponds to the san : "1. e4"
-import random #Use the generate random numbers for processing time
-import time #Used to time the executions
-import mss #Used to get superfast screenshots
-import os
 import glob
+import os
+
+import cv2  # OpenCV
 import game_state_classes
+import mss  # Used to get superfast screenshots
+import numpy as np
+import pyautogui  # Used to take screenshots and move the mouse
+
 
 def find_chessboard():
-    #We do a first classical screenshot to see the screen size:
+    # We do a first classical screenshot to see the screen size:
     screenshot_shape = np.array(pyautogui.screenshot()).shape
-    monitor = {'top': 0, 'left': 0, 'width': screenshot_shape[1], 'height': screenshot_shape[0]}
+    monitor = {
+        "top": 0,
+        "left": 0,
+        "width": screenshot_shape[1],
+        "height": screenshot_shape[0],
+    }
     sct = mss.mss()
     img = np.array(np.array(sct.grab(monitor)))
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    is_found, current_chessboard_image,minX,minY,maxX,maxY,test_image = find_chessboard_from_image(img)
-    position = game_state_classes.Board_position(minX,minY,maxX,maxY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    is_found, current_chessboard_image, minX, minY, maxX, maxY, test_image = (
+        find_chessboard_from_image(img)
+    )
+    position = game_state_classes.Board_position(minX, minY, maxX, maxY)
     return is_found, position
+
 
 def get_chessboard(game_state):
     position = game_state.board_position_on_screen
-    monitor = {'top': 0, 'left': 0, 'width': position.maxX + 10, 'height': position.maxY + 10}
+    monitor = {
+        "top": 0,
+        "left": 0,
+        "width": position.maxX + 10,
+        "height": position.maxY + 10,
+    }
     img = np.array(np.array(game_state.sct.grab(monitor)))
-    #Converting the image in grayscale:
-    image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    dim = (800, 800 ) # perform the actual resizing of the chessboard
-    resizedChessBoard = cv2.resize(image[position.minY:position.maxY, position.minX:position.maxX], dim, interpolation = cv2.INTER_AREA)
+    # Converting the image in grayscale:
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    dim = (800, 800)  # perform the actual resizing of the chessboard
+    resizedChessBoard = cv2.resize(
+        image[position.minY : position.maxY, position.minX : position.maxX],
+        dim,
+        interpolation=cv2.INTER_AREA,
+    )
     return resizedChessBoard
 
+
 def find_chessboard_from_image(img):
-    #The algorithm here is much faster than the previous one:
-    #1 Get the horizontal lines by convolving the image with [[-1,1]], get the indexes with the most start and end lines
-    #2 Get the vertical lines by convolving the image with [[-1],[1]], get the indexes with the most start and end lines
-    #3 Check if there is only one probable start and end for each dimension, and the board is squared
-    #4 Resize the image to a std dimension to standardize the treatments
+    # The algorithm here is much faster than the previous one:
+    # 1 Get the horizontal lines by convolving the image with [[-1,1]], get the indexes with the most start and end lines
+    # 2 Get the vertical lines by convolving the image with [[-1],[1]], get the indexes with the most start and end lines
+    # 3 Check if there is only one probable start and end for each dimension, and the board is squared
+    # 4 Resize the image to a std dimension to standardize the treatments
 
-    #Converting the image in grayscale:
-    image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Converting the image in grayscale:
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     found_board = False
-        
-    kernelH = np.array([[-1,1]])
-    kernelV = np.array([[-1],[1]])
 
-    #Récupération des lignes horizontales :
-    lignesHorizontales = np.absolute(cv2.filter2D(image.astype('float'),-1,kernelV))
-    ret,thresh1 = cv2.threshold(lignesHorizontales,30,255,cv2.THRESH_BINARY)
-    
-    kernelSmall = np.ones((1,3), np.uint8)
-    kernelBig = np.ones((1,50), np.uint8)
+    kernelH = np.array([[-1, 1]])
+    kernelV = np.array([[-1], [1]])
 
-    #Remove holes:
+    # Récupération des lignes horizontales :
+    lignesHorizontales = np.absolute(cv2.filter2D(image.astype("float"), -1, kernelV))
+    ret, thresh1 = cv2.threshold(lignesHorizontales, 30, 255, cv2.THRESH_BINARY)
+
+    kernelSmall = np.ones((1, 3), np.uint8)
+    kernelBig = np.ones((1, 50), np.uint8)
+
+    # Remove holes:
     imgH1 = cv2.dilate(thresh1, kernelSmall, iterations=1)
     imgH2 = cv2.erode(imgH1, kernelSmall, iterations=1)
-    
-    #Remove small lines
+
+    # Remove small lines
     imgH3 = cv2.erode(imgH2, kernelBig, iterations=1)
     imgH4 = cv2.dilate(imgH3, kernelBig, iterations=1)
 
-    linesStarts = cv2.filter2D(imgH4,-1,kernelH)
-    linesEnds = cv2.filter2D(imgH4,-1,-kernelH)
+    linesStarts = cv2.filter2D(imgH4, -1, kernelH)
+    linesEnds = cv2.filter2D(imgH4, -1, -kernelH)
 
-    lines = linesStarts.sum(axis=0)/255
+    lines = linesStarts.sum(axis=0) / 255
     lineStart = 0
     nbLineStart = 0
     for idx, val in enumerate(lines):
@@ -72,41 +88,41 @@ def find_chessboard_from_image(img):
             nbLineStart += 1
             lineStart = idx
 
-    lines = linesEnds.sum(axis=0)/255
+    lines = linesEnds.sum(axis=0) / 255
     lineEnd = 0
     nbLineEnd = 0
     for idx, val in enumerate(lines):
         if val > 6:
             nbLineEnd += 1
-            lineEnd = idx        
+            lineEnd = idx
 
-    #Récupération des lignes verticales:
-    lignesVerticales = np.absolute(cv2.filter2D(image.astype('float'),-1,kernelH))
-    ret,thresh1 = cv2.threshold(lignesVerticales,30,255,cv2.THRESH_BINARY)
-    
-    kernelSmall = np.ones((3,1), np.uint8)
-    kernelBig = np.ones((50,1), np.uint8)
-    
-    #Remove holes:
+    # Récupération des lignes verticales:
+    lignesVerticales = np.absolute(cv2.filter2D(image.astype("float"), -1, kernelH))
+    ret, thresh1 = cv2.threshold(lignesVerticales, 30, 255, cv2.THRESH_BINARY)
+
+    kernelSmall = np.ones((3, 1), np.uint8)
+    kernelBig = np.ones((50, 1), np.uint8)
+
+    # Remove holes:
     imgV1 = cv2.dilate(thresh1, kernelSmall, iterations=1)
     imgV2 = cv2.erode(imgV1, kernelSmall, iterations=1)
-    
-    #Remove small lines
+
+    # Remove small lines
     imgV3 = cv2.erode(imgV2, kernelBig, iterations=1)
     imgV4 = cv2.dilate(imgV3, kernelBig, iterations=1)
 
-    columnStarts = cv2.filter2D(imgV4,-1,kernelV)
-    columnEnds = cv2.filter2D(imgV4,-1,-kernelV)
+    columnStarts = cv2.filter2D(imgV4, -1, kernelV)
+    columnEnds = cv2.filter2D(imgV4, -1, -kernelV)
 
-    column = columnStarts.sum(axis=1)/255
+    column = columnStarts.sum(axis=1) / 255
     columnStart = 0
     nbColumnStart = 0
     for idx, val in enumerate(column):
         if val > 6:
             columnStart = idx
             nbColumnStart += 1
-            
-    column = columnEnds.sum(axis=1)/255
+
+    column = columnEnds.sum(axis=1) / 255
     columnEnd = 0
     nbColumnEnd = 0
     for idx, val in enumerate(column):
@@ -114,14 +130,18 @@ def find_chessboard_from_image(img):
             columnEnd = idx
             nbColumnEnd += 1
 
-
     found_board = False
-    if (nbLineStart == 1) and (nbLineEnd == 1) and (nbColumnStart == 1) and (nbColumnEnd == 1) :
+    if (
+        (nbLineStart == 1)
+        and (nbLineEnd == 1)
+        and (nbColumnStart == 1)
+        and (nbColumnEnd == 1)
+    ):
         print("We found a board")
         if abs((columnEnd - columnStart) - (lineEnd - lineStart)) > 3:
-            print ("However, the board is not a square")
+            print("However, the board is not a square")
         else:
-            print(columnStart,columnEnd,lineStart,lineEnd)
+            print(columnStart, columnEnd, lineStart, lineEnd)
             if (columnEnd - columnStart) % 8 == 1:
                 columnEnd -= 1
             if (columnEnd - columnStart) % 8 == 7:
@@ -130,23 +150,46 @@ def find_chessboard_from_image(img):
                 lineStart += 1
             if (lineEnd - lineStart) % 8 == 7:
                 lineStart -= 1
-            print(columnStart,columnEnd,lineStart,lineEnd)
+            print(columnStart, columnEnd, lineStart, lineEnd)
 
             found_board = True
     else:
         print("We did not found the borders of the board")
 
     if found_board:
-        print("Found chessboard sized:" , (columnEnd-columnStart),(lineEnd-lineStart)," x:",columnStart,columnEnd," y: ",lineStart,lineEnd)
-        dim = (800, 800 ) # perform the actual resizing of the chessboard
-        print(lineStart,lineEnd,columnStart,columnEnd)
-        resizedChessBoard = cv2.resize(image[columnStart:columnEnd, lineStart:lineEnd], dim, interpolation = cv2.INTER_AREA)
-        return True, resizedChessBoard , lineStart, columnStart  , lineEnd   , columnEnd  , resizedChessBoard 
+        print(
+            "Found chessboard sized:",
+            (columnEnd - columnStart),
+            (lineEnd - lineStart),
+            " x:",
+            columnStart,
+            columnEnd,
+            " y: ",
+            lineStart,
+            lineEnd,
+        )
+        dim = (800, 800)  # perform the actual resizing of the chessboard
+        print(lineStart, lineEnd, columnStart, columnEnd)
+        resizedChessBoard = cv2.resize(
+            image[columnStart:columnEnd, lineStart:lineEnd],
+            dim,
+            interpolation=cv2.INTER_AREA,
+        )
+        return (
+            True,
+            resizedChessBoard,
+            lineStart,
+            columnStart,
+            lineEnd,
+            columnEnd,
+            resizedChessBoard,
+        )
 
-    return False, image, 0, 0, 0, 0 , image
+    return False, image, 0, 0, 0, 0, image
 
-def test_chessboard_detection(imageDir,expectedBoard):
-    valid_image_extensions = [".jpg", ".jpeg", ".png", ".tif", ".tiff"] 
+
+def test_chessboard_detection(imageDir, expectedBoard):
+    valid_image_extensions = [".jpg", ".jpeg", ".png", ".tif", ".tiff"]
     image_count = 0
     error_count = 0
     for file in os.listdir(imageDir):
@@ -154,18 +197,21 @@ def test_chessboard_detection(imageDir,expectedBoard):
         extension = os.path.splitext(file)[1]
         if extension.lower() not in valid_image_extensions:
             continue
-        image_count = image_count+1
+        image_count = image_count + 1
         image = cv2.imread(os.path.join(imageDir, file))
-        found_board, resizedChessBoard , minX,minY,maxX,maxY, info_image =  find_chessboard_from_image(image)
+        found_board, resizedChessBoard, minX, minY, maxX, maxY, info_image = (
+            find_chessboard_from_image(image)
+        )
         if found_board != expectedBoard:
             print("Error in", file)
             error_count = error_count + 1
-            cv2.imwrite("Errors/Error" + repr(expectedBoard) + file,info_image)
+            cv2.imwrite("Errors/Error" + repr(expectedBoard) + file, info_image)
         else:
-            cv2.imwrite("Errors/NoError" + repr(expectedBoard) + file,info_image)
+            cv2.imwrite("Errors/NoError" + repr(expectedBoard) + file, info_image)
 
-    #print("Had ", error_count, " error_count out of ", image_count, " images")
-    return image_count,error_count
+    # print("Had ", error_count, " error_count out of ", image_count, " images")
+    return image_count, error_count
+
 
 def cleanFolder(folder_name):
     files = glob.glob(folder_name)
@@ -174,19 +220,24 @@ def cleanFolder(folder_name):
 
 
 def global_test_chessboard_detection():
-    cleanFolder('Errors/*')
-    
-    #Images having a board
+    cleanFolder("Errors/*")
+
+    # Images having a board
     print("\nTreating images having a board : ")
-    image_count,error_count = test_chessboard_detection("TestImages/withBoard/",True)
-    print("Errors with images having a board : ", 100 * error_count/image_count, "%")
-    
-    #Images having no board
+    image_count, error_count = test_chessboard_detection("TestImages/withBoard/", True)
+    print("Errors with images having a board : ", 100 * error_count / image_count, "%")
+
+    # Images having no board
     print("\nTreating images having no board : ")
-    image_count,error_count = test_chessboard_detection("TestImages/withoutBoard/",False)
-    print("Errors with images not having a board : ", 100 * error_count/image_count, "%")
-    
+    image_count, error_count = test_chessboard_detection(
+        "TestImages/withoutBoard/", False
+    )
+    print(
+        "Errors with images not having a board : ", 100 * error_count / image_count, "%"
+    )
+
     print("\n Please find outputs in the folder Errors/")
+
 
 if __name__ == "__main__":
     global_test_chessboard_detection()
--- code/main.py
+++ code/main.py
@@ -1,28 +1,34 @@
-#Pour l'export : pyinstaller -w -F -i .\Ressources\DG.ico .\interface_graphique.py
+# Pour l'export : pyinstaller -w -F -i .\Ressources\DG.ico .\interface_graphique.py
 import tkinter as tk
-import chessboard_detection
-import cv2 #OpenCV
-import board_basics
-from game_state_classes import *
 from tkinter.simpledialog import askstring
 
+import board_basics
+import chessboard_detection
+from game_state_classes import *
+
+
 def clear_logs():
-    logs_text.delete('1.0', tk.END)
-    #add_log("Logs have been cleared:")
+    logs_text.delete("1.0", tk.END)
+    # add_log("Logs have been cleared:")
+
 
 def add_log(log):
-    logs_text.insert(tk.END,log + "\n")
+    logs_text.insert(tk.END, log + "\n")
+
 
 def stop_playing():
     clear_logs()
-    button_start = tk.Button(text="Start playing - RESTART NOT WORKING YET", command =start_playing)
-    button_start.grid(column=0,row =1)
+    button_start = tk.Button(
+        text="Start playing - RESTART NOT WORKING YET", command=start_playing
+    )
+    button_start.grid(column=0, row=1)
+
 
 def start_playing():
     game_state = Game_state()
     add_log("Looking for a chessboard...")
 
-    found_chessboard, position  = chessboard_detection.find_chessboard()
+    found_chessboard, position = chessboard_detection.find_chessboard()
 
     if found_chessboard:
         add_log("Found the chessboard " + position.print_custom())
@@ -31,64 +37,69 @@ def start_playing():
         add_log("Could not find the chessboard")
         add_log("Please try again when the board is open on the screen\n")
         return
-    
 
-    button_start = tk.Button(text="Stop playing", command =stop_playing)
-    button_start.grid(column=0,row =1)
-
-
+    button_start = tk.Button(text="Stop playing", command=stop_playing)
+    button_start.grid(column=0, row=1)
 
     add_log("Checking if we are black or white...")
     resized_chessboard = chessboard_detection.get_chessboard(game_state)
-    #cv2.imshow('Resized image',resized_chessboard)
+    # cv2.imshow('Resized image',resized_chessboard)
     game_state.previous_chessboard_image = resized_chessboard
 
     we_are_white = board_basics.is_white_on_bottom(resized_chessboard)
     game_state.we_play_white = we_are_white
     if we_are_white:
-        add_log("We are white" )
+        add_log("We are white")
         game_state.moves_to_detect_before_use_engine = 0
     else:
         add_log("We are black")
         game_state.moves_to_detect_before_use_engine = 1
         first_move_registered = False
         while first_move_registered == False:
-            first_move_string = askstring('First move', 'What was the first move played by white?')
+            first_move_string = askstring(
+                "First move", "What was the first move played by white?"
+            )
             if len(first_move_string) > 0:
                 first_move = chess.Move.from_uci(first_move_string)
-                first_move_registered = game_state.register_move(first_move,resized_chessboard)
+                first_move_registered = game_state.register_move(
+                    first_move, resized_chessboard
+                )
 
-        add_log("First move played by white :"+ first_move_string)        
-    
+        add_log("First move played by white :" + first_move_string)
+
     while True:
         window.update()
-        #cv2.imshow('Resized image',game_state.previous_chessboard_image)
-        #add_log("Moves to detect before use engine" + str(game_state.moves_to_detect_before_use_engine))
+        # cv2.imshow('Resized image',game_state.previous_chessboard_image)
+        # add_log("Moves to detect before use engine" + str(game_state.moves_to_detect_before_use_engine))
         if game_state.moves_to_detect_before_use_engine == 0:
-            #add_log("Our turn to play:")
+            # add_log("Our turn to play:")
             game_state.play_next_move()
-            #add_log("We are done playing")
-        
+            # add_log("We are done playing")
+
         found_move, move = game_state.register_move_if_needed()
         if found_move:
             clear_logs()
             add_log("The board :\n" + str(game_state.board) + "\n")
             add_log("\nAll moves :\n" + str(game_state.executed_moves))
-    
+
 
 window = tk.Tk()
-#window.geometry("300x300")
+# window.geometry("300x300")
 window.title("ChessBot")
 
-label_titre = tk.Label(text="Welcome on my chessbot, hope you will have fun with it",anchor="e", wraplength = 300)#\nThis bot can not work on a game that already started")
-label_titre.grid(column = 0,row = 0)
+label_titre = tk.Label(
+    text="Welcome on my chessbot, hope you will have fun with it",
+    anchor="e",
+    wraplength=300,
+)  # \nThis bot can not work on a game that already started")
+label_titre.grid(column=0, row=0)
 
 
-button_start = tk.Button(text="Start playing", command =start_playing)
-button_start.grid(column=0,row =1)
+button_start = tk.Button(text="Start playing", command=start_playing)
+button_start.grid(column=0, row=1)
 
-logs_text = tk.Text(window,width=40,height=25,background='gray')
-logs_text.grid(column = 0,row = 2)
+logs_text = tk.Text(window, width=40, height=25, background="gray")
+logs_text.grid(column=0, row=2)
 
 
 window.mainloop()
